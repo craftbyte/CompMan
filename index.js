ca121c057c13447073b072f67be3c6a46085af69
Anže Jenšterle <anze.jensterle@castawaylabs.com>
2016-03-18 20:33:46 +0100

    Landing

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/static'));
+
 
 app.get('/', (req,res) => {
 	res.render('index')
 })
+
+app.use((req, res, next) => {
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT||3000, function () {
   console.log('Running on port '+(process.env.PORT||3000));
